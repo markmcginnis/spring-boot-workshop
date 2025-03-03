f34a73e7edc9925f1d093cee6101b3d73f47d62f
markmcginnis <mark.mcginnis32@gmail.com>
2025-03-03 15:14:42 -0500

    startup

--- src/main/java/org/example/SpringBootWorkshop.java
+++ src/main/java/org/example/SpringBootWorkshop.java
@@ -1,13 +1,16 @@
 package org.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.example.controller.PakudexController;
+import org.example.controller.PakuriController;
+import org.example.service.PakuriService;
 
-@SpringBootApplication
 public class SpringBootWorkshop {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWorkshop.class, args);
+        PakuriService pakuriService = new PakuriService();
+        PakudexController pakudexController = new PakudexController(pakuriService);
+        PakuriController pakuriController = new PakuriController(pakuriService);
+        // serve stuff
     }
 
 //    public static void main(String[] args) {
