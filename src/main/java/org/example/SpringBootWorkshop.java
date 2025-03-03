29df775ec87c2041f5f71382885f350b1665eeca
markmcginnis <mark.mcginnis32@gmail.com>
2025-03-03 16:05:15 -0500

    controller fixes

--- src/main/java/org/example/SpringBootWorkshop.java
+++ src/main/java/org/example/SpringBootWorkshop.java
@@ -1,14 +1,13 @@
 package org.example;
 
-import org.example.controller.PakudexController;
-import org.example.controller.PakuriController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringBootWorkshop {
 
     public static void main(String[] args) {
-        PakudexController pakudexController = new PakudexController();
-        PakuriController pakuriController = new PakuriController();
-        // serve stuff
+        SpringApplication.run(SpringBootWorkshop.class, args);
     }
 
 //    public static void main(String[] args) {
