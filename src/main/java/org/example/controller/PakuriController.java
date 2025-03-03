880c5f76b394ba3710f71712553f42adcd553220
markmcginnis <mark.mcginnis32@gmail.com>
2025-03-03 16:37:08 -0500

    make them beans

--- src/main/java/org/example/controller/PakuriController.java
+++ src/main/java/org/example/controller/PakuriController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.example.models.Pakuri;
 import org.example.service.PakuriService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 @RequestMapping("/pakuri")
 public class PakuriController {
 
+    @Autowired
     private PakuriService pakuriService;
 
     @GetMapping
