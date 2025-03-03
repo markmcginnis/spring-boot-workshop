9c69a3d8f2aaaadb2e9f3904ae3b7a95aeb3938f
markmcginnis <mark.mcginnis32@gmail.com>
2025-03-03 16:37:08 -0500

    make them beans

--- src/main/java/org/example/controller/PakudexController.java
+++ src/main/java/org/example/controller/PakudexController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.example.models.Pakudex;
 import org.example.service.PakuriService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pakudex")
 public class PakudexController {
 
+    @Autowired
     private PakuriService pakuriService;
 
     @GetMapping
