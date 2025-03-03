40a15658d25717bcb3f3cc4ccfeb630d7af6afc9
markmcginnis <mark.mcginnis32@gmail.com>
2025-03-03 15:06:04 -0500

    set pakudex in pakudex controller

--- src/main/java/org/example/controller/PakudexController.java
+++ src/main/java/org/example/controller/PakudexController.java
@@ -4,6 +4,8 @@ import org.example.models.Pakudex;
 import org.example.service.PakuriService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/pakudex")
@@ -14,6 +16,15 @@ public class PakudexController {
     @GetMapping
     private ResponseEntity<Pakudex> getPakudex() {
         return ResponseEntity.ok(pakuriService.getAsPakudex());
+    }
+
+    @PostMapping
+    private ResponseEntity<Boolean> setPakudex(@RequestBody Pakudex pakudex) {
+        boolean failure = pakudex.getPakuriMap()
+            .values()
+            .stream()
+            .anyMatch(pakuri -> !pakuriService.updatePakuri(pakuri));
+        return ResponseEntity.ok(failure);
     }
 
 }
